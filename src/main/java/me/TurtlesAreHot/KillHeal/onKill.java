0f734ca093aa1c89c6d665b612cfc720200a27f4
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-12-04 12:03:28 -0500

    Add files via upload

--- src/main/java/me/TurtlesAreHot/KillHeal/onKill.java
+++ src/main/java/me/TurtlesAreHot/KillHeal/onKill.java
@@ -5,15 +5,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class onKill implements Listener {
 
     @EventHandler
     public void onPlayerKill(PlayerDeathEvent e) {
         Player killer = e.getEntity().getKiller();
-        if(killer instanceof Player) {
-            killer.sendMessage(ChatColor.GRAY + "You had " + killer.getHealth() + " left. You were healed for killing someone.");
-            killer.setHealth(20.0);
+        if(killer != null) {
+            Player killed = e.getEntity();
+            if (!(killer.getUniqueId().equals(killed.getUniqueId()))) {
+                killer.sendMessage(ChatColor.GRAY + "You had " + killer.getHealth() + " left. You were healed for killing someone.");
+                killer.setHealth(20.0);
+            }
         }
     }
 }
